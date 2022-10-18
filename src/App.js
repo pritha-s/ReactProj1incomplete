aef8109a4674554c03baf5bf3493c6c7138441aa
pritha-s <107198524+pritha-s@users.noreply.github.com>
2022-10-18 21:51:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Navbar from './components/Navbar';
+
+
+function App() {
+  return (
+    <>  
+<Navbar  title="TextUtils4" aboutText="About TextUtils"/>
+    </>
+  );
+}
+
+export default App;
---
+++ src/module2.mjs
@@ -0,0 +1,9 @@
+const a= 'Pritha';
+const b='Harry';
+const c='Hermione';
+const d='Ronald';
+
+export default c;
+export {a}
+export {b}
+export {d}
